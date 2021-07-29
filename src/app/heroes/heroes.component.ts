d58b00ef4d252eb17919a452792bcabc5df1e6cc
Bilal Waqar <bilal.waqar@outlook.com>
2021-07-29 22:44:27 +0500

    message-service integrated

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/hero';
 import { HeroService } from '../../services/hero-service/hero.service';
-
+import { MessageService } from 'src/services/message-service/message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
   heroes: Hero[] = [];
   selectedHero?: Hero;
-  async ngOnInit() {
-    const abc = this.getHeroes();
-    console.log("after getHeroes");
+  ngOnInit() {
+    this.getHeroes();
   }
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
-  async getHeroes() {
-    this.heroes = await this.heroService.getHeroes();
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe({
+      next: (heroes) => (this.heroes = heroes),
+      complete: () => console.log('getHeroes complete'),
+      error: (error) => console.log('getHeroes', error),
+    });
   }
 }
--- src/services/hero-service/hero.service.ts
+++ src/services/hero-service/hero.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../../app/interfaces/hero';
 import { HEROES } from '../../app/mock-data/mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from '../message-service/message.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  constructor(private messageService: MessageService) {}
 
-  constructor() { }
-
-  async getHeroes(): Promise<Hero[]> {
-
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(HEROES);
-      }, 5000);
-    });
-
+  // async getHeroes(): Promise<Hero[]> {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve(HEROES);
+  //     }, 5000);
+  //   });
+  // }
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 }
