146b9f861e47f2eeb409958131ce68d8f4436314
Bilal Waqar <bilal.waqar@outlook.com>
2021-07-30 17:52:42 +0500

    Do Search, add, edit, delete heroes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -23,11 +23,16 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id).subscribe(
       (hero) => (this.hero = hero),
-      error => console.log,
+      (error) => console.log,
       () => {}
     );
   }
